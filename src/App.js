1de95f690e1752dace27ddb6580a150a4a75a91c
Ganesh Chandran <cganesh8@gmail.com>
2020-06-02 08:53:51 +1000

    Updated Nav and Car icon

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import { Route, Switch } from "react-router-dom";
 import './App.css';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
 
-      <Header />
+          <div className='Content'>
+            <h1>This is the H1 text in App.js</h1>
+            <p>This is the para text in Content div</p>
+          </div>
 
-      </header>
-    </div>
-  );
+        <Switch>
+          <Route path="/Home" component={Home} exact />
+          <Route path="/" />
+          <Route path="/" />
+        </Switch>
+                
+      </div>
+    );
+  }
 }
 
 export default App;
